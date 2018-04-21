394873a3057db1b8b7d1ea4d4a4d2e25c5f691c7
Pontus Rohdén <Pontusrohden@hotmail.com>
2018-04-21 14:36:29 +0200

    Slide meny

---
+++ src/OpenHack/NavigationDrawer/app/src/main/java/com/example/pontu/navigationdrawer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pontu.navigationdrawer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.exempelmenu, menu);
+        return true;
+    }
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        mToggle=new ActionBarDrawerToggle(MainActivity.this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
